5b4c5a127b10dbbf730a22c7edcfa2bc7bbbcadb
Allison <farrella95@gmail.com>
2023-02-27 10:03:02 -0500

    finished i think

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let hamburger = document.querySelector(".hamburger-img");
 let mobileMenu = document.querySelector(".mobile-menu");
 let close = document.querySelector(".x-logo");
+let background = document.querySelector(".overlay");
 
 hamburger.addEventListener("click", () => {
   mobileMenu.style.transform = "translateX(0%)";
+  background.style.display = "block";
 });
 
 close.addEventListener("click", () => {
   mobileMenu.style.transform = "translateX(100%)";
+  background.style.display = "none";
 });
